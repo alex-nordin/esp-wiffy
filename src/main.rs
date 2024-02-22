fae270da5261dd184ce7804588994920389547db
Alex Nordin <alex_m_nordin@pm.me>
2024-02-22 20:39:55 +0100

    Setup basic wifi connection
    Try to connect to local wifi. Test connection with tcp socket

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,156 @@
 #![no_std]
 #![no_main]
+#![feature(type_alias_impl_trait)]
 
-use esp32c3_hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, Delay};
+use embassy_executor::Spawner;
+use embassy_net::{tcp::TcpSocket, Config, Ipv4Address, Stack, StackResources};
+use embassy_time::{Duration, Timer};
+use embedded_svc::wifi::{ClientConfiguration, Configuration, Wifi};
 use esp_backtrace as _;
 use esp_println::println;
+use esp_wifi::{
+    initialize,
+    wifi::{WifiController, WifiDevice, WifiStaDevice, WifiState},
+    EspWifiInitFor,
+};
+use hal::{
+    clock::ClockControl, embassy, peripherals::Peripherals, prelude::*, timer::TimerGroup, Rng,
+};
+use static_cell::make_static;
 
-#[entry]
-fn main() -> ! {
+const SSID: &str = env!("SSID");
+const PASSWORD: &str = env!("PASSWORD");
+
+#[main]
+async fn main(spawner: Spawner) -> ! {
+    println!("SSID set as: {}", SSID);
+    println!("PASSWORD set as: {}", PASSWORD);
     let peripherals = Peripherals::take();
     let system = peripherals.SYSTEM.split();
 
     let clocks = ClockControl::max(system.clock_control).freeze();
-    let mut delay = Delay::new(&clocks);
+    let timer = hal::systimer::SystemTimer::new(peripherals.SYSTIMER).alarm0;
+    let wifi_init = initialize(
+        EspWifiInitFor::Wifi,
+        timer,
+        Rng::new(peripherals.RNG),
+        system.radio_clock_control,
+        &clocks,
+    )
+    .unwrap();
 
-    println!("Hello world!");
-    loop {
-        println!("Loop...");
-        delay.delay_ms(500u32);
+    let wifi = peripherals.WIFI;
+    let (wifi_interface, controller) =
+        esp_wifi::wifi::new_with_mode(&wifi_init, wifi, WifiStaDevice).unwrap();
+
+    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
+    embassy::init(&clocks, timer_group0);
+
+    let dhcp_conf = Config::dhcpv4(Default::default());
+
+    let seed = 9128;
+
+    let stack = &*make_static!(Stack::new(
+        wifi_interface,
+        dhcp_conf,
+        make_static!(StackResources::<3>::new()),
+        seed
+    ));
+
+    match spawner.spawn(connection(controller)) {
+        Ok(()) => println!("spawning connection task... are we still connected to wifi?"),
+        Err(e) => println!("{e:?}"),
     }
+    // spawner.spawn(connection(controller)).ok();
+    // spawner.spawn(net_task(&stack)).ok();
+    match spawner.spawn(net_task(&stack)) {
+        Ok(()) => println!("net task ran fine"),
+        Err(e) => println!("{e:?}"),
+    }
+
+    loop {
+        if stack.is_link_up() {
+            break;
+        }
+        Timer::after(Duration::from_millis(500)).await;
+    }
+
+    loop {
+        if let Some(config) = stack.config_v4() {
+            println!("Got assigned an IP: {}", config.address);
+            break;
+        }
+        Timer::after(Duration::from_millis(500)).await;
+    }
+
+    let mut rx_buffer = [0; 4096];
+    let mut tx_buffer = [0; 4096];
+    loop {
+        let mut socket = TcpSocket::new(&stack, &mut rx_buffer, &mut tx_buffer);
+        socket.set_timeout(Some(embassy_time::Duration::from_secs(10)));
+
+        let localhost = (Ipv4Address::new(127, 0, 0, 1), 1883);
+        println!("connecting to local socket: localhost:1882");
+        let local_socket = socket.connect(localhost).await;
+        if let Err(e) = local_socket {
+            println!("connection error {:?}", e);
+            continue;
+        }
+        // println!("connected, lets test by posting to socket");
+        // let mut buf = [0; 1024];
+        loop {
+            use embedded_io_async::Write;
+            let r = socket
+                .write_all(b"I am speaking to you through a socket")
+                .await;
+            if let Err(e) = r {
+                println!("write error: {:?}", e);
+                break;
+            }
+            Timer::after(Duration::from_millis(3000)).await;
+        }
+    }
+    // loop {}
+}
+
+#[embassy_executor::task]
+async fn connection(mut controller: WifiController<'static>) {
+    println!("trying to connect ");
+    println!("Device capabilities {:?}", controller.get_capabilities());
+    loop {
+        match esp_wifi::wifi::get_wifi_state() {
+            WifiState::StaConnected => {
+                controller
+                    .wait_for_event(esp_wifi::wifi::WifiEvent::StaDisconnected)
+                    .await;
+                Timer::after(Duration::from_millis(5000)).await;
+            }
+            _ => {}
+        }
+        if !matches!(controller.is_started(), Ok(true)) {
+            let client_conf = Configuration::Client(ClientConfiguration {
+                ssid: SSID.try_into().unwrap(),
+                password: PASSWORD.try_into().unwrap(),
+                ..Default::default()
+            });
+            controller.set_configuration(&client_conf).unwrap();
+            println!("starting wifi");
+            controller.start().await.unwrap();
+            println!("Wifi Started");
+        }
+        println!("Looks like we're about to connect!");
+
+        match controller.connect().await {
+            Ok(_) => println!("Connected to Wifi!"),
+            Err(e) => {
+                println!("Failed to connect: {e:?}");
+                Timer::after(Duration::from_millis(5000)).await
+            }
+        }
+    }
+}
+
+#[embassy_executor::task]
+async fn net_task(stack: &'static Stack<WifiDevice<'static, WifiStaDevice>>) {
+    stack.run().await
 }
